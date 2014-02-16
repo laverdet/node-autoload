187fcc603a029455b4fe3d4ed204de4004eda1a8
Marcel Laverdet <marcel@laverdet.com>
2014-02-15 22:27:30 -0600

    Support for cache file

--- autoload.js
+++ autoload.js
@@ -99,7 +99,41 @@ function registerGlobal(name, fn) {
  *   scope: {}, // Scope in which symbols should be defined
  * }
  */
-function autoload(root, loaders, ondone) {
+function autoload(root, loaders, cachePath, ondone) {
+	if (!ondone) {
+		ondone = cachePath;
+		cachePath = undefined;
+	}
+	var cache = Object.create(null);
+
+	function injectSymbols(path, loader, symbols) {
+		for (var ii = 0; ii < symbols.length; ++ii) {
+			// It's possible the symbol has already been defined, in which case we should avoid
+			// clobbering it with the autoloader.
+			if (!loader.scope.hasOwnProperty(symbols[ii])) {
+				~function(loader, symbol) {
+					Object.defineProperty(loader.scope, symbol, {
+						get: function() {
+							delete loader.scope[symbol];
+							loader.require(path);
+							if (loader.scope.hasOwnProperty(symbol)) {
+								return loader.scope[symbol];
+							} else {
+								throw new Error('Failed to autoload ' + symbol);
+							}
+						},
+						set: function(val) {
+							delete loader.scope[symbol];
+							loader.scope[symbol] = val;
+						},
+						configurable: true,
+						enumerable: true,
+					});
+				}(loader, symbols[ii]);
+			}
+		}
+	}
+
 	function autoloader(path, ondone) {
 		var fullPath = path ? joinPath(root, path) : root;
 
@@ -120,48 +154,43 @@ function autoload(root, loaders, ondone) {
 					return ondone();
 				}
 
-				// Read the file from disk to pass to parsers
-				fs.readFile(fullPath, 'utf-8', function(err, source) {
-					if (err) {
-						throw new Error(err);
-					}
-
+				// Exists in cache?
+				if (fullPath in cache && cache[fullPath].mtime === +stat.mtime) {
 					for (var ii = 0; ii < loaders.length; ++ii) {
 						if (loaders[ii].pattern.test(path)) {
 							// Parse and register autoloading loaders
-							var symbols = loaders[ii].parse(source, path);
-							for (var jj = 0; jj < symbols.length; ++jj) {
-								// It's possible the symbol has already been defined, in which case we should avoid
-								// clobbering it with the autoloader.
-								if (!loaders[ii].scope.hasOwnProperty(symbols[jj])) {
-									~function(loader, symbol) {
-										Object.defineProperty(loader.scope, symbol, {
-											get: function() {
-												delete loader.scope[symbol];
-												loader.require(path);
-												if (loader.scope.hasOwnProperty(symbol)) {
-													return loader.scope[symbol];
-												} else {
-													throw new Error('Failed to autoload ' + symbol);
-												}
-											},
-											set: function(val) {
-												delete loader.scope[symbol];
-												loader.scope[symbol] = val;
-											},
-											configurable: true,
-											enumerable: true,
-										});
-									}(loaders[ii], symbols[jj]);
-								}
-							}
+							var symbols = cache[fullPath].symbols[ii];
+							injectSymbols(path, loaders[ii], symbols);
 						}
 					}
-
 					ondone();
-				});
+				} else {
 
-			} else if (stat.isDirectory()) {
+					// Read the file from disk to pass to parsers
+					fs.readFile(fullPath, 'utf-8', function(err, source) {
+						if (err) {
+							throw new Error(err);
+						}
+						cache[fullPath] = {
+							mtime: +stat.mtime,
+							symbols: [],
+						};
+
+						for (var ii = 0; ii < loaders.length; ++ii) {
+							if (loaders[ii].pattern.test(path)) {
+								// Parse and register autoloading loaders
+								var symbols = loaders[ii].parse(source, path);
+								cache[fullPath].symbols.push(symbols);
+								injectSymbols(path, loaders[ii], symbols);
+							} else {
+								cache[fullPath].symbols.push(null);
+							}
+						}
+						ondone();
+					});
+				}
+
+			} else if (stat.isDirectory() && path !== 'node_modules') {
 				// Descend directory
 				fs.readdir(fullPath, function(err, files) {
 					if (err) {
@@ -194,5 +223,19 @@ function autoload(root, loaders, ondone) {
 			}
 		});
 	}
-	autoloader(false, ondone);
+	if (cachePath) {
+		fs.readFile(cachePath, 'utf-8', function(err, val) {
+			if (!err) {
+				try {
+					cache = JSON.parse(val);
+					cache.__proto__ = null;
+				} catch (err) {}
+			}
+			autoloader(false, function(err, val) {
+				fs.writeFile(cachePath, JSON.stringify(cache), 'utf-8', ondone);
+			});
+		});
+	} else {
+		autoloader(false, ondone);
+	}
 }
